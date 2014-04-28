d58ac4ad6d78b4dd6a4dbff7ed50a62d40b55ac0
AlCh- <nalpzor@gmail.com>
2014-04-28 19:36:04 +0300

    Loop try
    Loop 1st try and rendering

--- src/GraphicManager.java
+++ src/GraphicManager.java
@@ -1,5 +1,8 @@
 //Drawing logic goes here - what to draw, where
+import java.awt.*;
 public class GraphicManager {
+
+	private BufferedImage img = new BufferedImage(StartGame.WINDOW_WIDTH,StartGame.WINDOW_HEIGHT, BufferedImage.Type_INT_RGB)
 
 	GraphicPanel lnkGraphicPanel;
 	World lnkWorld;
@@ -9,9 +12,23 @@ public class GraphicManager {
 		this.lnkGraphicPanel = lnkGraphicPanel;
 		this.lnkWorld = lnkWorld;
 	}
+
+	Graphics g = img.getGraphics(); 
+
+	public void render();{
 	
-	public void render()
-	{
-		//Kwsta diko sou
+	//g.drawImage(img, int x, int y, Image Observer)
+	//lnkworld.currentscene.tiles
+	for(int i = 0; i < tiles.length; i++){
+	for(int j = 0; j < tiles[].length; j++){
+
+	//to-do: draw the tiles	
+	
+
+			}
+		}
+
+
 	}
+
 }
--- src/World.java
+++ src/World.java
@@ -1,4 +1,6 @@
 import java.awt.Dimension;
+import java.awt.*;
+import java.awt.Image.*;
 
 //Central class - holds everything from the game either directly (e.g. Scenes) or indirectly (e.g. Scenes.Enemies)
 //Acts like a hub in the game logic, mostly everything points to here
@@ -30,8 +32,40 @@ public class World {
 	public void gameLoop()
 	{
 		//Kwsta diko sou
+		//Time
+		private boolean isRunning;
+		isRunning = true;
+		int FPS = 60.0;
+		double IdealTime = 1000000000 / FPS;
+		long initialTime = System.nanoTime();
+		long Delta = differenceTime / IdealTime; 
+
+		while(isRunning);{
+		long now = System.nanoTime();
+		differenceTime = now - initialTime;
+		initialTime = now;
+	//	endTime += differenceTime;
+		
+		updateGame();	
+
+		//rendering
+		render();{
+
+		}
+
+/*		try{Thread.sleep(1){
+	
+
+
+		catch(Exception ex) {
+		
+		system.out.println("Ooops!")
+
+ 		}
+	}*/
+	
 	}
-	
-	
+
+}
 
 }
